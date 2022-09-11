79eba9f8e9b0965374caf6fc2358cbba41e1c887
wealthwissy <wealthwissy69@gmail.com>
2022-09-11 12:35:02 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - entry point
+ * Return: 0 success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * Description: main - this shit is hard
+ * Return: 0 if success
+ */
+int main(void)
+{
+printf("with proper grammar,  but the outcome is a piece of art,\n");
+}
