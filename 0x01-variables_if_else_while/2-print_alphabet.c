91260db09ad028fbd11268e2412263ac99d20eef
wealthwissy <wealthwissy69@gmail.com>
2022-09-12 17:22:23 -0700

    2-print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,10 @@
 */
 int main(void)
 {
-	char ch = 'a';
+char ch = 'a';
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	     putchar(ch);
-	putchar('\n');
-	return (0);
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
 }
