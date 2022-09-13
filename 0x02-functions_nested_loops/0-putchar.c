a4097d1a04d033c83987b38acca75bc78dee2a4b
wealthwissy <wealthwissy69@gmail.com>
2022-09-13 13:07:13 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char wisdom[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(wisdom[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
