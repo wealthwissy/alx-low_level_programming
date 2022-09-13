33b6a568e7daa0fd93a8df296439cf7111c3865d
wealthwissy <wealthwissy69@gmail.com>
2022-09-12 18:43:26 -0700

    9-print

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible combinations of single-digit
+ * Numbers must be serperated by ,followed by a space
+ * Numbers should be printed in ascending order
+ * You can only use the putchar function
+ * Return: 0
+*/
+
+int main(void)
+{
+int d = 0;
+
+while (d < 10)
+{
+putchar(d + '0');
+if (d < 9)
+{
+putchar(',');
+putchar(' ');
+}
+d++;
+}
+putchar('\n');
+return (0);
+}
