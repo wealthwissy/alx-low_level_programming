d9fda797a034288a3dbb967b7f09d041d4cb8e3d
wealthwissy <wealthwissy69@gmail.com>
2022-10-01 01:47:35 +0100

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - checks if a given
+ * char is a number or not
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+unsigned int i, sum, num;
+sum = 0;
+if (argc < 3)
+{
+printf("%d\n", 0);
+return (0);
+}
+while (argc-- && argc > 0)
+{
+for (i = 0; argv[argc][i] != '\0'; i++)
+{
+if (!(isdigit(argv[argc][i])))
+{
+printf("Error\n");
+return (1);
+}
+}
+num = atoi(argv[argc]);
+sum += num;
+}
+printf("%d\n", sum);
+return (sum);
+}
