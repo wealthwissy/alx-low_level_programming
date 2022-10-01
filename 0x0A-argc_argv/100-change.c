46327842a2d12022b24fc417ce55a84a2b8c6626
wealthwissy <wealthwissy69@gmail.com>
2022-10-01 01:57:32 +0100

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,48 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 /**
- * main - prints the minimum number of coins
- * to make change for an amount of money
- * @argc: n args
- * @argv: arr args
- * Return: 0
+ * main - function
+ * @argc: length of argv
+ * @argv: number of argument
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-int value, c;
-c = 0;
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-value = atoi(argv[1]);
-if (value < 0)
-{
-printf("%d\n", 0);
-return (0);
-}
-if (value % 25 >= 0)
-{
-c += value / 25;
-value = value % 25;
-}
-if (value % 5 >= 0)
-{
-c += value / 5;
-value = value % 5;
-}
-if (value % 2 >= 0)
-{
-c += value / 2;
-value = value % 2;
-}
-if (value % 1 >= 0)
-{
-c += value / 1;
-}
-printf("%d\n", c);
-return (0);
+	/*Declaring variables*/
+	int position, total, change, aux;
+	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+
+	position = total = change = aux = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	total = atoi(argv[1]); /*Covert str to int*/
+
+	if (total <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	/*Declaring While*/
+	while (coins[position] != '\0')
+	{
+		if (total >= coins[position])
+		{
+			aux = (total / coins[position]);
+			change += aux;
+			total -= coins[position] * aux;
+		}
+
+		position++;
+	}
+	printf("%d\n", change);
+
+	return(0);
 }
